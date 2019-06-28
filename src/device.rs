liujing2/vmm-device

vm-device: Use DeviceManager to manage the device instance
DeviceManager struct is the top level device management to record
the device instance topology and device resource range mapping for
dispatching to the corresponding device instance when VM exit.

According to vm-memory crate, Range struct is defined as {GuestAddress, GuestUsize}
instead of {u64, u64}.

Signed-off-by: Jing Liu <jing2.liu@linux.intel.com>

src/device.rs
@@ -3,7 +3,8 @@
 
 //! Handles routing to devices in an address space.
 use std::string::String;
-use vm_memory::GuestAddress;
+use std::sync::{Arc, Mutex};
+use vm_memory::{GuestAddress, GuestUsize};
 
 /// Trait for devices with basic functions.
 #[allow(unused_variables)]
@@ -14,6 +15,11 @@ pub trait Device: Send {
     fn read(&mut self, addr: GuestAddress, data: &mut [u8], io_type: IoType);
     /// Write `data` to the guest physical address `addr`.
     fn write(&mut self, addr: GuestAddress, data: &[u8], io_type: IoType);
+    /// Set the allocated resource to device.
+    ///
+    /// This will be called by DeviceManager::register_device() to set
+    /// the allocated resource from the vm_allocator back to device.
+    fn set_resources(&mut self, res: &[Resource]);
 }
 
 /// Resource type.
@@ -25,4 +31,55 @@ pub enum IoType {
     Mmio,
     /// Non-exit physically backed mmap IO
     PhysicalMmio,
+}
+
+/// Device resource information.
+#[derive(Debug, Copy, Clone)]
+pub struct Resource {
+    /// Resource address.
+    pub addr: Option<GuestAddress>,
+    /// Resource size.
+    pub size: GuestUsize,
+    /// Resource type.
+    pub res_type: IoType,
+}
+
+impl Resource {
+    /// Build a Resource struct.
+    pub fn new(addr: Option<GuestAddress>, size: GuestUsize, res_type: IoType) -> Resource {
+        Resource {
+            addr,
+            size,
+            res_type,
+        }
+    }
+}
+
+/// Storing Device information and for topology managing by name.
+pub struct DeviceDescriptor {
+    /// Device name.
+    pub name: String,
+    /// The device to descript.
+    pub device: Arc<Mutex<dyn Device>>,
+    /// The parent bus of this device.
+    pub parent_bus: Option<Arc<Mutex<dyn Device>>>,
+    /// Device resource set.
+    pub resource: Vec<Resource>,
+}
+
+impl DeviceDescriptor {
+    /// Create a descriptor for one device.
+    pub fn new(
+        name: String,
+        dev: Arc<Mutex<dyn Device>>,
+        parent_bus: Option<Arc<Mutex<dyn Device>>>,
+        resource: Vec<Resource>,
+    ) -> Self {
+        DeviceDescriptor {
+            name,
+            device: dev,
+            parent_bus,
+            resource,
+        }
+    }
 }

liujing2/vm-device

src/device.rs
@@ -3,7 +3,8 @@
 
 //! Handles routing to devices in an address space.
 use std::string::String;
-use vm_memory::GuestAddress;
+use std::sync::{Arc, Mutex};
+use vm_memory::{GuestAddress, GuestUsize};
 
 /// Trait for devices with basic functions.
 #[allow(unused_variables)]
@@ -14,6 +15,11 @@ pub trait Device: Send {
     fn read(&mut self, addr: GuestAddress, data: &mut [u8], io_type: IoType);
     /// Write `data` to the guest physical address `addr`.
     fn write(&mut self, addr: GuestAddress, data: &[u8], io_type: IoType);
+    /// Set the allocated resource to device.
+    ///
+    /// This will be called by DeviceManager::register_device() to set
+    /// the allocated resource from the vm_allocator back to device.
+    fn set_resources(&mut self, res: &[Resource]);
 }
 
 /// Resource type.
@@ -25,4 +31,55 @@ pub enum IoType {
     Mmio,
     /// Non-exit physically backed mmap IO
     PhysicalMmio,
+}
+
+/// Device resource information.
+#[derive(Debug, Copy, Clone)]
+pub struct Resource {
+    /// Resource address.
+    pub addr: Option<GuestAddress>,
+    /// Resource size.
+    pub size: GuestUsize,
+    /// Resource type.
+    pub res_type: IoType,
+}
+
+impl Resource {
+    /// Build a Resource struct.
+    pub fn new(addr: Option<GuestAddress>, size: GuestUsize, res_type: IoType) -> Resource {
+        Resource {
+            addr,
+            size,
+            res_type,
+        }
+    }
+}
+
+/// Storing Device information and for topology managing by name.
+pub struct DeviceDescriptor {
+    /// Device name.
+    pub name: String,
+    /// The device to descript.
+    pub device: Arc<Mutex<dyn Device>>,
+    /// The parent bus of this device.
+    pub parent_bus: Option<Arc<Mutex<dyn Device>>>,
+    /// Device resource set.
+    pub resources: Vec<Resource>,
+}
+
+impl DeviceDescriptor {
+    /// Create a descriptor for one device.
+    pub fn new(
+        name: String,
+        dev: Arc<Mutex<dyn Device>>,
+        parent_bus: Option<Arc<Mutex<dyn Device>>>,
+        resources: Vec<Resource>,
+    ) -> Self {
+        DeviceDescriptor {
+            name,
+            device: dev,
+            parent_bus,
+            resources,
+        }
+    }
 }