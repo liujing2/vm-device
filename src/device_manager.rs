liujing2/vmm-device

vm-device: Rename Resource to IoResource
Resource structure only describes IO resource information.
Rename it to make it clear.

Signed-off-by: Jing Liu <jing2.liu@linux.intel.com>

src/device_manager.rs
@@ -100,13 +100,13 @@ impl<'a> DeviceManager<'a> {
         &self,
         dev: Arc<Mutex<dyn Device>>,
         parent_bus: Option<Arc<Mutex<dyn Device>>>,
-        resource: Vec<Resource>,
+        resource: Vec<IoResource>,
     ) -> DeviceDescriptor {
         let name = dev.lock().expect("Failed to require lock").name();
         DeviceDescriptor::new(name, dev.clone(), parent_bus, resource)
     }
 
-    fn allocate_resources(&mut self, resource: &mut Vec<Resource>) -> Result<()> {
+    fn allocate_resources(&mut self, resource: &mut Vec<IoResource>) -> Result<()> {
         let mut alloc_idx = 0;
 
         for res in resource.iter_mut() {
@@ -140,7 +140,7 @@ impl<'a> DeviceManager<'a> {
         Err(Error::Overlap)
     }
 
-    fn free_resources(&mut self, resource: &[Resource]) {
+    fn free_resources(&mut self, resource: &[IoResource]) {
         for res in resource.iter() {
             match res.res_type {
                 IoType::Pio => self.resource.free_io_addresses(res.addr.unwrap(), res.size),
@@ -154,7 +154,7 @@ impl<'a> DeviceManager<'a> {
     fn register_resource(
         &mut self,
         dev: Arc<Mutex<dyn Device>>,
-        resource: &mut Vec<Resource>,
+        resource: &mut Vec<IoResource>,
     ) -> Result<()> {
         for res in resource.iter() {
             match res.res_type {
@@ -187,7 +187,7 @@ impl<'a> DeviceManager<'a> {
         &mut self,
         dev: Arc<Mutex<dyn Device>>,
         parent_bus: Option<Arc<Mutex<dyn Device>>>,
-        resource: &mut Vec<Resource>,
+        resource: &mut Vec<IoResource>,
     ) -> Result<()> {
         // Reserve resource
         if let Err(e) = self.allocate_resources(resource) {
@@ -337,7 +337,7 @@ mod tests {
             ///
             /// This will be called by DeviceManager::register_device() to set
             /// the allocated resource from the vm_allocator back to device.
-            fn set_resources(&mut self, _res: &[Resource]) {}
+            fn set_resources(&mut self, _res: &[IoResource]) {}
         }
         impl BusDevice {
             pub fn new(name: String) -> Self {
@@ -346,9 +346,9 @@ mod tests {
                     config_address: 0x1000,
                 }
             }
-            pub fn get_resource(&self) -> Vec<Resource> {
+            pub fn get_resource(&self) -> Vec<IoResource> {
                 let mut req_vec = Vec::new();
-                let res = Resource::new(Some(GuestAddress(0xcf8)), 8 as GuestUsize, IoType::Pio);
+                let res = IoResource::new(Some(GuestAddress(0xcf8)), 8 as GuestUsize, IoType::Pio);
 
                 req_vec.push(res);
                 req_vec

liujing2/vm-device

src/device_manager.rs
@@ -98,7 +98,7 @@ impl DeviceManager {
         &self,
         dev: Arc<dyn Device>,
         parent_bus: Option<Arc<dyn Device>>,
-        resources: Vec<Resource>,
+        resources: Vec<IoResource>,
     ) -> DeviceDescriptor {
         DeviceDescriptor::new(dev.name(), dev.clone(), parent_bus, resources)
     }
@@ -106,7 +106,7 @@ impl DeviceManager {
     // Allocate IO resources.
     // In order to transport the SystemAllocator Error, return Err with
     // the failure allocated index, or else return Ok().
-    fn allocate_io_resources(&mut self, resources: &mut Vec<Resource>) -> Result<()> {
+    fn allocate_io_resources(&mut self, resources: &mut Vec<IoResource>) -> Result<()> {
         for (idx, res) in resources.iter_mut().enumerate() {
             match res.res_type {
                 IoType::Pio => {
@@ -132,7 +132,7 @@ impl DeviceManager {
     }
 
     // Free valid `resources` which means all entries have a valid address.
-    fn free_io_resources(&mut self, resources: &[Resource]) {
+    fn free_io_resources(&mut self, resources: &[IoResource]) {
         for res in resources.iter() {
             // The resources addresses being free should not be None.
             let addr = res.try_unwrap();
@@ -149,7 +149,11 @@ impl DeviceManager {
     // Register IO resources.
     // Return the failure registering index when fails,
     // or else return resources length.
-    fn register_resources(&mut self, dev: Arc<dyn Device>, resources: &mut Vec<Resource>) -> usize {
+    fn register_resources(
+        &mut self,
+        dev: Arc<dyn Device>,
+        resources: &mut Vec<IoResource>,
+    ) -> usize {
         for (idx, res) in resources.iter().enumerate() {
             // The resources addresses being registered are sucessfully allocated before.
             let addr = res.try_unwrap();
@@ -184,7 +188,7 @@ impl DeviceManager {
         &mut self,
         dev: Arc<dyn Device>,
         parent_bus: Option<Arc<dyn Device>>,
-        resources: &mut Vec<Resource>,
+        resources: &mut Vec<IoResource>,
     ) -> Result<()> {
         // Reserve resources
         if let Err(Error::IoResourceAllocate(idx, e)) = self.allocate_io_resources(resources) {
@@ -216,7 +220,7 @@ impl DeviceManager {
     }
 
     // Unregister resources with all entries addresses valid.
-    fn unregister_resources(&mut self, resources: &[Resource]) {
+    fn unregister_resources(&mut self, resources: &[IoResource]) {
         for res in resources.iter() {
             // The resources addresses being unregistered is sucessfully allocated before.
             let addr = res.try_unwrap();